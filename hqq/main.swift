afe873faef15ea5dd70b13877a95402743d9d585
moxiaoyuan <hqq1@admindeiMac-32.local>
2016-03-07 20:09:32 +0800

    func add
    add(10,20)

--- hqq/main.swift
+++ hqq/main.swift
@@ -6,9 +6,30 @@
 //  Copyright (c) 2016年 hqq1. All rights reserved.
 //
 
+//无参函数调用
+/*
+import Cocoa
+func hello(){
+    print("hello!");
+}
+hello()
+hello()
+*/
+
+//有参函数调用
+/*
+import Cocoa
+func hello(num:Int){
+    print("hello!\(num)");
+}
+for var i in 1...10{
+    hello(i)
+}
+*/
 
 import Cocoa
-var i:Int
-for i=1;i<10;i++ {
-    print("\(i)");
+func add(x:Int,y:Int){
+    print("\(x)+\(y)=\(x+y)")
 }
+add(10,20)
+//add(10,y:20)
